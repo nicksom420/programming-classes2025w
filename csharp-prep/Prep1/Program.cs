83d97a6592a836db3cae8207a8720175c9e14fc0
Nick Sommer <som22006@byui.edu>
2025-01-11 20:34:59 -0700

    Finished C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? "); //Displays first question to user
+        string firstName = Console.ReadLine(); //Takes input from user and stores it as variable "firstName"
+
+        Console.Write("What is your last name? "); //Displayes second question to user
+        string lastName = Console.ReadLine(); //Takes input from user and stores it as variable "lastName"
+
+        Console.Write(Environment.NewLine);//Adds extra line to separate responses from results
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}."); //Displays both vairables into a string as specified in course instructions
     }
 }
