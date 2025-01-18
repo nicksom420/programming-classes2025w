99d991e0c6da5ed6fb55ae0906ab7a26f0e6a69b
Nick Sommer <som22006@byui.edu>
2025-01-17 17:15:49 -0700

    Corrected the range from 1-100

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
+        int number = randomGenerator.Next(1,101);
 
         Console.WriteLine("Welcome to the number guessing game!");
 
