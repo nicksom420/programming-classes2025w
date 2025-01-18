dfed6d4b4dfcd35f683705041c54594c05e4c15e
Nick Sommer <som22006@byui.edu>
2025-01-17 17:13:27 -0700

    finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+
+        Console.WriteLine("Welcome to the number guessing game!");
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+    
+            Console.Write("What is your guess? ");
+            
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number){
+                Console.WriteLine("Guess Lower");
+            }
+            else if (guess < number){
+                Console.WriteLine("Guess Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+            
+        }
+
     }
 }
