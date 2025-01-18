4cecc223dd4de7ca601e73fde09194e38f9f713a
Nick Sommer <som22006@byui.edu>
2025-01-17 18:17:50 -0700

    Finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int number = PromptUserNumber();
+
+        int numberSquared = SquareNumber(number);
+
+        DisplayResult(numberSquared, number, name);
+
+
     }
-}
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber( int number)
+    {
+        int squaredNumber = number*number;
+
+        return squaredNumber;
+
+    }
+
+    static void DisplayResult(int squaredNumber, int number, string userName)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        
+    }
+}   
