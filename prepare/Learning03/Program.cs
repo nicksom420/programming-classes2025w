5562deac099040e73354e4227482c55431b7dd75
Nick Sommer <som22006@byui.edu>
2025-02-07 20:31:28 -0700

    Finished learning assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstFraction =  new Fraction();
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+
+        Fraction secondFraction = new Fraction(5);
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+
+        Fraction thirdFraction = new Fraction(3, 4);
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+
+        Fraction fourthFraction = new Fraction(1, 3);
+        Console.WriteLine(fourthFraction.GetFractionString());
+        Console.WriteLine(fourthFraction.GetDecimalValue());
+        
+
     }
 }
