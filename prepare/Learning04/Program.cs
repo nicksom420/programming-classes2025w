07551206fe9e1b61032e71ca29f6f11ced61030d
Nick Sommer <som22006@byui.edu>
2025-02-21 16:49:01 -0700

    Finished learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,27 @@
-using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Test Assignment class
+        //Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        //string display = myAssignment.GetSummary();
+        //Console.WriteLine(display);
+
+        // Test MathAssignment class
+        //MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez","Fractions","Section 7.3","Problems 8-19");
+        //string summary = myMathAssignment.GetSummary();
+        //Console.WriteLine(summary);
+        //string homework = myMathAssignment.GetHomeworkList();
+        //Console.WriteLine(homework);
+
+        // Testing WritignAssignment Class
+        //WritingAssignment myWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        //string summary = myWritingAssignment.GetSummary();
+        //Console.WriteLine(summary);
+        //string writing = myWritingAssignment.GetWritingInformation();
+        //Console.WriteLine(writing);
+        
+
     }
 }
