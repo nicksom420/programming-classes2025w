11e767327830674223bfb1b599361d8c9b80225f
Nick Sommer <som22006@byui.edu>
2025-03-29 22:11:12 -0600

    added shell for score class

--- final/FinalProject/Snake Game/Score.cs
+++ final/FinalProject/Snake Game/Score.cs
@@ -1,4 +1,17 @@
 public class Score 
 {
-    
+    private int _userScore;
+
+    private int _highScore;
+
+    public void updateScore()
+    {
+
+    }
+
+    public void DisplayScore()
+    {
+        
+    }
+
 }
