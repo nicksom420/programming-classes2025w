178b3c6ed3fe52e2b61226f8493f2948d7ed0418
Nick Sommer <som22006@byui.edu>
2025-03-29 22:10:14 -0600

    added shells to empty classes

--- final/FinalProject/Snake Game/Food.cs
+++ final/FinalProject/Snake Game/Food.cs
@@ -1,4 +1,25 @@
-public class Food
+public class Food: Entity
 {
-    
+
+    private string _apple;
+
+    public void RandomPosition()
+    {
+
+    }
+
+    public override void Render()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void UpdatePosition()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Collisions()
+    {
+        throw new NotImplementedException();
+    }
 }
